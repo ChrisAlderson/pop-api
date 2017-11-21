04b2f5940795cc85a0e73f848b07e46205748594
Chris Alderson <chrisalderson@protonmail.com>
2017-11-21 09:43:15 +0100

    add api error tests

---
+++ test/helpers/ApiError.spec.js
@@ -0,0 +1,28 @@
+// Import the necessary modules.
+// @flow
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai'
+
+import ApiError from '../../src/helpers/ApiError'
+import statusCodes from '../../src/helpers/statusCodes'
+
+/** @test {ApiError} */
+describe('ApiError', () => {
+  /** @test {ApiError#constructor} */
+  it('should create a new ApiError', () => {
+    const apiError = new ApiError({
+      message: 'test message'
+    })
+    expect(apiError).to.be.an('Error')
+  })
+
+  /** @test {ApiError#constructor} */
+  it('should create a new ApiError without default parameters', () => {
+    const apiError = new ApiError({
+      message: 'test message',
+      status: statusCodes.NOT_FOUND,
+      isPublic: true
+    })
+    expect(apiError).to.be.an('Error')
+  })
+})
