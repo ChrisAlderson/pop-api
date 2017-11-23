af7cffe2e66a579c62aa9100bdfdfcbe9842fb88
Chris Alderson <chrisalderson@protonmail.com>
2017-11-23 09:05:45 +0100

    use export from syntax

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
-// Import the necessary modules.
-// @flow
-import {
-  Cli,
-  Database,
-  HttpServer,
-  Logger,
-  Routes
-} from './middleware'
-import {
-  BaseContentController,
-  ContentService,
-  IContentController,
-  IController
-} from './controllers'
-import {
-  ApiError,
-  statusCodes
-} from './helpers'
-
-import PopApi from './PopApi'
-import * as utils from './utils'
-
-/**
- * Object with modules to expose.
- * @type {Object}
- * @ignore
- */
-// const expose: Object = {
-export {
-  Cli,
-  Database,
-  HttpServer,
-  Logger,
-  Routes,
-  BaseContentController,
-  ContentService,
-  IContentController,
-  IController,
-  ApiError,
-  statusCodes,
-  PopApi,
-  utils
-}
+// Export the necessary modules.
+export PopApi from './PopApi'
+export * from './controllers'
+export * from './helpers'
+export * from './middleware'
+export * as utils from './utils'
